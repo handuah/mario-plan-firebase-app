fccb1b39efd1bf018510dec72523353c7f2ae1e3
Hannah Duah <handuah@gmail.com>
2019-06-27 17:22:24 -1200

    added CreateProject.js list

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Dashboard from './components/dashboard/Dashboard.js';
 import ProjectDetails from './components/projects/ProjectDetails.js';
 import SignIn from './components/auth/SignIn.js';
 import SignUp from './components/auth/SignUp.js';
+import CreateProject from './components/projects/CreateProject.js';
 
 
 class App extends React.Component{
@@ -19,6 +20,7 @@ class App extends React.Component{
                     <Route path='/project/:id' component={ProjectDetails}></Route>
                     <Route path='/signin' component={SignIn}></Route>
                     <Route path='/signup' component={SignUp}></Route>
+                    <Route path='/create' component={CreateProject}></Route>
                 </Switch>
             </div>
         </BrowserRouter>
