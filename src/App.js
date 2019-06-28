c8c183d06d44e78fcef9a20cc9c2c44f9fd391d4
Hannah Duah <handuah@gmail.com>
2019-06-27 16:07:48 -1200

    added ProjectList component and Dashboard Links

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Navbar from './components/layout/Navbar.js';
+import Dashboard from './components/dashboard/Dashboard.js';
+
+
 class App extends React.Component{
     render(){
     return (
         <BrowserRouter>
             <div className="App">
                 <Navbar />
+
+                <Switch>
+                    <Route path='/' component={Dashboard}></Route>
+                </Switch>
             </div>
         </BrowserRouter>
   )
