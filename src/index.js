10443947a6a5faab176bb5125274361e89e06d0d
Hannah Duah <handuah@gmail.com>
2019-06-28 11:49:04 -1200

    imported rooReducer, createstore and Provider

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+
 import './index.css';
 import App from './App';
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore } from 'redux';
+import rootReducer from './stores/reducers/rootReducer';
+import {Provider} from 'react-redux';
+
+
+const marioStore = createStore(rootReducer);
+
+
+ReactDOM.render(<Provider store = {marioStore}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
