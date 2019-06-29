42ddca41589ca83dc4a8bc815644ad19964cb018
Hannah Duah <handuah@gmail.com>
2019-06-28 12:40:59 -1200

    imported connect from react-redux

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,16 +1,20 @@
 import React, {Component} from 'react';
 import Notifications from './Notifications.js';
 import ProjectList from '../projects/ProjectList.js';
+import { connect } from 'react-redux';
 
 class Dashboard extends React.Component {
+
     render(){
+
+        const { projects } = this.props;
 
         return(
             <div className="dashboard container">
 
                 <div className="row">
                     <div className="col s12 m6">
-                        <ProjectList />
+                        <ProjectList projectsToDisplay = {projects}/>
                     </div>
 
                     <div className="col s12 m5 offset-m1">
@@ -22,4 +26,10 @@ class Dashboard extends React.Component {
     }
 }
 
-export default Dashboard
+const mapProjectStateToProps = (state) => {
+    return{
+        projects: state.project.projectsWritten
+    }
+}
+
+export default connect(mapProjectStateToProps)(Dashboard)
