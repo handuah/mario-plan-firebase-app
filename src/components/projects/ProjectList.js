7b2837f7ecf7cc51616440de55678a2fc7815e2b
Hannah Duah <handuah@gmail.com>
2019-06-28 12:41:55 -1200

    passed down project props dynamically on dummy data

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import ProjectSummary from './ProjectSummary.js';
 
-const ProjectList = () => {
+const ProjectList = ({projectsToDisplay}) => {
     return(
         <div className="project-list secton">
-            <ProjectSummary />
-            <ProjectSummary />
-            <ProjectSummary />
-            <ProjectSummary />
+            {projectsToDisplay && projectsToDisplay.map(project => {
+                return(
+                    <ProjectSummary project = {project} key= {project.id}/> 
+                )
+            })}
+            
         </div>
     )
 } 
--- src/stores/reducers/projectReducer.js
+++ src/stores/reducers/projectReducer.js
@@ -1,4 +1,10 @@
-const initState = {}
+const initState = {
+    projectsWritten: [
+        {id: '1' , title: 'Hannah loves music', content: 'You have absolutely no idea'},
+        {id: '2' , title: 'Hannah chases dreams', content: 'You have absolutely no idea'},
+        {id: '3' , title: 'Working my but off here', content: 'You have absolutely no idea'}
+    ]
+}
 
 const projectReducer = (state = initState, action) => {
     return state
