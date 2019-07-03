1fd607e2d2eb5649426cbe12add61e8d089340f9
Hannah Duah <handuah@gmail.com>
2019-07-02 12:11:29 -1200

    no changes made

--- src/stores/reducers/rootReducer.js
+++ src/stores/reducers/rootReducer.js
@@ -1,8 +1,14 @@
 import { combineReducers } from 'redux'
+import { firebaseReducer } from 'react-redux-firebase'
+import { firestoreReducer } from 'redux-firestore'
 import authReducer from './authReducer'
 import projectReducer from './projectReducer'
 
+export const initialState = {}
+
 const rootReducer = combineReducers({
+    firebase: firebaseReducer,
+    firestore: firestoreReducer,
     auth: authReducer,
     project: projectReducer
 });
