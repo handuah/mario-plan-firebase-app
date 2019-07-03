92eb039a99a03ff9f3afe98f1fb6f063946a28b4
Hannah Duah <handuah@gmail.com>
2019-07-02 12:09:24 -1200

    imported todosRef and added data to firestore

--- src/stores/actions/projectActions.js
+++ src/stores/actions/projectActions.js
@@ -1,7 +1,24 @@
+import { todosRef } from '../../config/firebaseConfig' //importing firestore instance
+
 export const createProject = (project) => {
     return(dispatch, getState, { getFirebase, getFirestore }) => {
         //make async call
+        // const firestoreData = getFirestore;
 
-        dispatch({type: 'CREATE_PROJECT', project : project});
+        //adding data to our firestore database using a promise
+        todosRef.collection('projects').add({
+            ...project,
+            authorFirstName: "Hannah",
+            authorLastName: "Duah",
+            authorId: "123",
+            createdAt: new Date()
+        }).then(() => {
+            dispatch({type: 'CREATE_PROJECT', project : project});
+        }).catch((error) => {
+            dispatch({type: 'CREATE_PROJECT_ERROR', error});
+        })
+
+        // todosRef.collection('projects');
+        
     }
 }
