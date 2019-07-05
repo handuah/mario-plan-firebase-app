2b828ea0718349f41b9ca895a65fca2cab0b40e5
Hannah Duah <handuah@gmail.com>
2019-07-04 16:26:26 -1200

    passed state to props and connected firestore props to the component essentially wrapping the data around the component

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,23 +1,77 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { firestoreConnect } from 'react-redux-firebase'
+import { compose } from 'redux'
 
 const ProjectDetails  = (props) => {
 
-    const id = props.match.params.id;   
-
-    return (
-        <div className="container section project-details">
+   // const id = props.match.params.id;   
+    const { project } = props;
+    if (project) {
+        return(
+          <div className="container section project-details">
             <div className="card z-depth-0">
                 <div className="card-content">
-                    <span className="card-title">Project Title - {id}</span>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo quae necessitatibus in earum ratione corporis magnam aut, provident vel perspiciatis inventore, deleniti, voluptas ullam consequuntur vero accusantium quaerat officia totam.</p>
+                    <span className="card-title">{project.title}</span>
+                    <p>{project.content}</p>
                     <div className="card-action grey lighten-4 grey-text">
-                        <p>Posted by Hannah Duah</p>
+                        <p>Posted by {project.authorFirstname}</p>
                         <p>4th May, 2am</p>
                     </div>
                 </div>
             </div>
         </div>
+        )
+    } else{
+    return (
+      <div className="container center">
+        <p>Loading...</p>
+      </div>
     )
+    }
 }
 
-export default ProjectDetails
+const mapToStateToProps = (state, ownProps) => {
+    console.log(state);
+    const id = ownProps.match.params.id;
+    const projectsinFirestore = state.firestore.data.projects;
+    const projectClicked = projectsinFirestore ? projectsinFirestore[id] : null
+    return {
+        //project: projectClicked  -- 
+           project: state.firestore.data[projectClicked] && state.firestore.data.project
+    }
+}
+
+export default compose(
+    connect(mapToStateToProps),
+    firestoreConnect([
+        { collection: 'projects'}
+    ])
+) (ProjectDetails)
+
+
+
+//Cleaner way to fetch a specific project document from projects collection:
+
+//const mapStateToProps = (state) => {
+ //    return {
+  //        project: state.firestore.data[project] && state.firestore.data.project
+          // Not state.firestore.ordered as a single project document will be fetched so technically there is no order needed.
+          // If you still want state.firestore.ordered, you can try the following:        
+          // project: state.firestore.ordered.project[0]
+          // But be aware that ordered will have just one document so you will need extra [0] 
+ //    }
+//}
+
+//export default compose(
+ //    connect(
+ //         mapStateToProps
+ //    ),
+ //    firestoreConnect(props => {
+
+          // console.log("firestoreConnect props are the same that are passed to the component, ", props);
+ //         return [
+ //              { collection: "projects", doc: props.match.params.id, storeAs: "project" },
+ //         ];
+ //    })
+//)(Quiz);
